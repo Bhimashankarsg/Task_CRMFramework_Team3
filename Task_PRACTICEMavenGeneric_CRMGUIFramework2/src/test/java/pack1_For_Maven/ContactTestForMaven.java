c398be83c984b38b39dec5f1e41f70030a1baa14
Shankar S G <Bhimashankarsg>
2024-11-08 15:42:28 +0530

    uploaded Task_CRMProject

---
+++ Task_PRACTICEMavenGeneric_CRMGUIFramework2/src/test/java/pack1_For_Maven/ContactTestForMaven.java
@@ -0,0 +1,16 @@
+package pack1_For_Maven;
+
+import org.testng.annotations.Test;
+
+public class ContactTestForMaven {
+@Test
+	public void createcontactTest()
+	{
+		System.out.println("Execute createcontactTest");
+	}
+	@Test
+	public void modifycontactTest()
+	{
+		System.out.println("Execute modifycontactTest");
+	}
+}
---
+++ Task_PRACTICEMavenGeneric_CRMGUIFramework2/src/test/java/pack2_For_Maven/OrgTestForMaven.java
@@ -0,0 +1,17 @@
+package pack2_For_Maven;
+
+import org.testng.annotations.Test;
+
+public class OrgTestForMaven {
+	
+@Test
+	public void createOrgTest()
+	{
+		System.out.println("Execute createOrgTest");
+	}
+	@Test
+	public void modifyOrgtTest()
+	{
+		System.out.println("Execute modifyOrgtTest");
+	}
+}
